22f7ef4c1be5569c3be53bf35a04c343cd9288d6
Joaquim Campos <joaquimcampos15@hotmail.com>
2021-07-29 19:32:09 +0200

    refactor(datasets): linting

--- deepsplines/datasets.py
+++ deepsplines/datasets.py
@@ -19,8 +19,8 @@ def init_dataset(**params):
     """
 
     # add your datasets here and create a corresponding Dataset class
-    dataset_dict = {'s_shape' : S_shape, 'circle' : Circle,
-                    'cifar10' : Cifar10, 'cifar100' : Cifar100, 'mnist' : MNIST}
+    dataset_dict = {'s_shape': S_shape, 'circle': Circle,
+                    'cifar10': Cifar10, 'cifar100': Cifar100, 'mnist': MNIST}
 
     if params['dataset_name'] not in dataset_dict.keys():
         raise ValueError('Chosen dataset is not available... Exiting.')
@@ -30,12 +30,11 @@ def init_dataset(**params):
     return dataset
 
 
-
 class Dataset(ABC):
     """ Abstract class for datasets """
 
     def __init__(self, dataset_name=None, log_dir=None, model_name=None,
-                plot_imgs=False, save_imgs=False, **kwargs):
+                 plot_imgs=False, save_imgs=False, **kwargs):
         """
         Args:
             dataset_name (str):
@@ -58,25 +57,20 @@ class Dataset(ABC):
         self.save_imgs = save_imgs
         self.get_plot = plot_imgs or save_imgs
 
-
-
     @abstractproperty
     def is_user_dataset(self):
         """ True if the dataset is user-defined (bool) """
         pass
-
 
     @abstractproperty
     def num_classes(self):
         """ Number of classes in the dataset (int) """
         pass
 
-
     @abstractmethod
     def plot_train_imgs(self, *args, **kwargs):
         """ Plots/saves train images """
         pass
-
 
     @property
     def get_test_imgs(self):
@@ -85,42 +79,37 @@ class Dataset(ABC):
         return self.get_plot and self.is_user_dataset
 
 
-
 class TorchDataset(Dataset):
     """ Abstract class for Torchvision datasets """
 
     def __init__(self, **params):
         """ """
         super().__init__(**params)
-        self._is_user_dataset = False # torchvision dataset
-        self.sample_batch = 4 # show 4 sample images
-
-
+        self._is_user_dataset = False  # torchvision dataset
+        self.sample_batch = 4  # show 4 sample images
 
     @property
     def is_user_dataset(self):
         """ """
         return self._is_user_dataset
 
-
     @abstractproperty
     def norm_mean(self):
         """ Mean for each channel in the training dataset (tuple). """
         pass
 
-
     @abstractproperty
     def norm_std(self):
-        """ Standard deviation for each channel in the training dataset (tuple). """
+        """
+        Standard deviation for each channel in the training
+        dataset (tuple).
+        """
         pass
-
 
     @abstractproperty
     def classes(self):
         """ Names of the classes (tuple). """
         pass
-
-
 
     def get_minimal_transform_list(self):
         """
@@ -133,8 +122,6 @@ class TorchDataset(Dataset):
 
         return minimal_transform_list
 
-
-
     @abstractmethod
     def get_augment_transform_list(self):
         """
@@ -144,8 +131,6 @@ class TorchDataset(Dataset):
         """
         pass
 
-
-
     def get_train_valid_transforms(self):
         """ Get training and validation dataset transforms. """
 
@@ -153,12 +138,11 @@ class TorchDataset(Dataset):
         train_transform_list = self.get_augment_transform_list()
         minimal_transform_list = self.get_minimal_transform_list()
 
-        train_transform = transforms.Compose(train_transform_list + minimal_transform_list)
+        train_transform = transforms.Compose(
+            train_transform_list + minimal_transform_list)
         valid_transform = transforms.Compose(minimal_transform_list)
 
         return train_transform, valid_transform
-
-
 
     def get_test_transform(self):
         """ Get training and validation dataset transforms. """
@@ -168,14 +152,10 @@ class TorchDataset(Dataset):
 
         return test_transform
 
-
-
     @abstractmethod
     def get_torchvision_dataset(self):
         """ Returns a torchvision dataset """
         pass
-
-
 
     def plot_train_imgs(self, trainloader):
         """
@@ -187,13 +167,15 @@ class TorchDataset(Dataset):
                 iterator of input images-label pairs.
         """
         data_iter = iter(trainloader)
-        images, labels = data_iter.next() # get first batch
-        images = denormalize(images, self.norm_mean, self.norm_std).clamp(min=0, max=1)
-        self.plot_torch_dataset_samples(images[0:self.sample_batch], labels[0:self.sample_batch])
+        images, labels = data_iter.next()  # get first batch
+        images = denormalize(images, self.norm_mean, self.norm_std)\
+            .clamp(min=0, max=1)
 
+        self.plot_torch_dataset_samples(
+            images[0:self.sample_batch], labels[0:self.sample_batch])
 
-
-    def plot_torch_dataset_samples(self, images, true_labels, pred_labels=None):
+    def plot_torch_dataset_samples(self, images, true_labels,
+                                   pred_labels=None):
         """
         Plot images/labels from a torchvision dataset.
 
@@ -206,9 +188,12 @@ class TorchDataset(Dataset):
             pred_labels (list):
                 list of predicted labels.
         """
-        assert math.sqrt(images.size(0)) % 1 == 0, 'plot_images expects a batch size which is a perfect power'
+        assert math.sqrt(images.size(0)) % 1 == 0, \
+            'plot_images expects a batch size which is a perfect power'
+
         H = int(math.sqrt(images.size(0)))
-        images = images.numpy().transpose([0, 2, 3, 1]) # convert to numpy format (N x H x W x C)
+        # convert to numpy format (N x H x W x C)
+        images = images.numpy().transpose([0, 2, 3, 1])
         fig, axes = plt.subplots(H, H)
 
         for i, ax in enumerate(axes.flat):
@@ -231,7 +216,6 @@ class TorchDataset(Dataset):
         plt.show()
 
 
-
 class Cifar10(TorchDataset):
     """ Class for CIFAR10 Dataset """
 
@@ -240,11 +224,9 @@ class Cifar10(TorchDataset):
         super().__init__(**params)
         self._num_classes = 10
         self._norm_mean = (0.4914, 0.4822, 0.4465)
-        self._norm_std  = (0.2470, 0.2435, 0.2616)
+        self._norm_std = (0.2470, 0.2435, 0.2616)
         self._classes = ('airplane', 'automobile', 'bird', 'cat', 'deer',
-                        'dog', 'frog', 'horse', 'ship', 'truck')
-
-
+                         'dog', 'frog', 'horse', 'ship', 'truck')
 
     @property
     def num_classes(self):
@@ -266,8 +248,6 @@ class Cifar10(TorchDataset):
         """ """
         return self._classes
 
-
-
     def get_augment_transform_list(self):
         """ Gets list of training augmentation transforms
         """
@@ -276,12 +256,9 @@ class Cifar10(TorchDataset):
 
         return train_transform_list
 
-
-
     def get_torchvision_dataset(self):
         """ """
         return torchvision.datasets.CIFAR10
-
 
 
 class Cifar100(TorchDataset):
@@ -292,29 +269,29 @@ class Cifar100(TorchDataset):
         super().__init__(**params)
         self._num_classes = 100
         self._norm_mean = (0.5072, 0.4867, 0.4412)
-        self._norm_std  = (0.2673, 0.2564, 0.2762)
+        self._norm_std = (0.2673, 0.2564, 0.2762)
         self._classes = ('apple', 'aquarium_fish', 'baby', 'bear', 'beaver',
-                        'bed', 'bee', 'beetle', 'bicycle', 'bottle',
-                        'bowl', 'boy', 'bridge', 'bus', 'butterfly',
-                        'camel', 'can', 'castle', 'caterpillar', 'cattle',
-                        'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach',
-                        'couch', 'crab', 'crocodile', 'cup', 'dinosaur',
-                        'dolphin', 'elephant', 'flatfish', 'forest', 'fox',
-                        'girl', 'hamster', 'house', 'kangaroo', 'keyboard',
-                        'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard',
-                        'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain',
-                        'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid',
-                        'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree',
-                        'plain', 'plate', 'poppy', 'porcupine', 'possum',
-                        'rabbit', 'raccoon', 'ray', 'road', 'rocket',
-                        'rose', 'sea', 'seal', 'shark', 'shrew',
-                        'skunk', 'skyscraper', 'snail', 'snake', 'spider',
-                        'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table',
-                        'tank', 'telephone', 'television', 'tiger', 'tractor',
-                        'train', 'trout', 'tulip', 'turtle', 'wardrobe',
-                        'whale', 'willow_tree', 'wolf', 'woman', 'worm')
-
-
+                         'bed', 'bee', 'beetle', 'bicycle', 'bottle',
+                         'bowl', 'boy', 'bridge', 'bus', 'butterfly',
+                         'camel', 'can', 'castle', 'caterpillar', 'cattle',
+                         'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach',
+                         'couch', 'crab', 'crocodile', 'cup', 'dinosaur',
+                         'dolphin', 'elephant', 'flatfish', 'forest', 'fox',
+                         'girl', 'hamster', 'house', 'kangaroo', 'keyboard',
+                         'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard',
+                         'lobster', 'man', 'maple_tree', 'motorcycle',
+                         'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange',
+                         'orchid', 'otter', 'palm_tree', 'pear',
+                         'pickup_truck', 'pine_tree', 'plain', 'plate',
+                         'poppy', 'porcupine', 'possum',
+                         'rabbit', 'raccoon', 'ray', 'road', 'rocket',
+                         'rose', 'sea', 'seal', 'shark', 'shrew',
+                         'skunk', 'skyscraper', 'snail', 'snake', 'spider',
+                         'squirrel', 'streetcar', 'sunflower', 'sweet_pepper',
+                         'table', 'tank', 'telephone', 'television',
+                         'tiger', 'tractor', 'train', 'trout',
+                         'tulip', 'turtle', 'wardrobe',
+                         'whale', 'willow_tree', 'wolf', 'woman', 'worm')
 
     @property
     def num_classes(self):
@@ -335,8 +312,6 @@ class Cifar100(TorchDataset):
     def classes(self):
         """ """
         return self._classes
-
-
 
     def get_augment_transform_list(self):
         """ Gets list of training augmentation transforms
@@ -346,12 +321,9 @@ class Cifar100(TorchDataset):
 
         return train_transform_list
 
-
-
     def get_torchvision_dataset(self):
         """ """
         return torchvision.datasets.CIFAR100
-
 
 
 class MNIST(TorchDataset):
@@ -361,10 +333,11 @@ class MNIST(TorchDataset):
         super().__init__(**params)
         self._num_classes = 10
         self._norm_mean = (0.1307,)
-        self._norm_std  = (0.3081,)
-        self._classes = ('0 - zero', '1 - one', '2 - two', '3 - three', '4 - four',
-                        '5 - five', '6 - six', '7 - seven', '8 - eight', '9 - nine')
-
+        self._norm_std = (0.3081,)
+        self._classes = ('0 - zero', '1 - one', '2 - two',
+                         '3 - three', '4 - four', '5 - five',
+                         '6 - six', '7 - seven', '8 - eight',
+                         '9 - nine')
 
     @property
     def num_classes(self):
@@ -386,16 +359,13 @@ class MNIST(TorchDataset):
         """ """
         return self._classes
 
-
     def get_augment_transform_list(self):
         """ """
         return []
 
-
     def get_torchvision_dataset(self):
         """ """
         return torchvision.datasets.MNIST
-
 
 
 class twoD(Dataset):
@@ -411,21 +381,18 @@ class twoD(Dataset):
         self._num_classes = 2
         self.rect_side = 2
         self.test_grid = 0.01
-        self.grid_arange = torch.arange(-1 + self.test_grid/2, 1, self.test_grid)
-
+        self.grid_arange = \
+            torch.arange(-1 + self.test_grid / 2, 1, self.test_grid)
 
     @property
     def is_user_dataset(self):
         """ """
         return self._is_user_dataset
 
-
     @property
     def num_classes(self):
         """ """
         return self._num_classes
-
-
 
     @abstractmethod
     def get_labels(self, inputs):
@@ -439,8 +406,6 @@ class twoD(Dataset):
         """
         pass
 
-
-
     def generate_set(self, nb_samples):
         """
         Generate training or validation dataset.
@@ -448,39 +413,34 @@ class twoD(Dataset):
         Args:
             num_samples (int).
         """
-        inputs = torch.empty(nb_samples, 2).uniform_(-self.rect_side/2, self.rect_side/2)
+        inputs = torch.empty(nb_samples, 2)\
+            .uniform_(-self.rect_side / 2, self.rect_side / 2)
+
         labels = self.get_labels(inputs)
 
         return inputs, labels
-
-
 
     def get_test_set(self):
         """ Generate test dataset """
 
         grid_x, grid_y = torch.meshgrid(self.grid_arange, self.grid_arange)
 
-        inputs = torch.cat((grid_x.reshape(-1, 1), grid_y.reshape(-1, 1)), dim=1)
+        inputs = torch.cat(
+            (grid_x.reshape(-1, 1), grid_y.reshape(-1, 1)), dim=1)
         labels = self.get_labels(inputs)
 
         return inputs, labels
 
-
-
     def init_plot_dict(self, mode='test'):
         """ Init plot dictionary """
 
-        return {'inputs' : [], 'outputs' : []}
-
-
+        return {'inputs': [], 'outputs': []}
 
     def add_to_plot_dict(self, plot_dict, batch_data):
         """ plot_dict = {'inputs' : list, 'outputs': list} """
 
         plot_dict['inputs'].append(batch_data[0])
         plot_dict['outputs'].append(batch_data[1])
-
-
 
     def concatenate_plot_dict(self, plot_dict):
         """ Concatenate plot dictionary lists """
@@ -489,8 +449,6 @@ class twoD(Dataset):
         outputs = torch.cat(plot_dict['outputs'], dim=0)
 
         return inputs, outputs
-
-
 
     @abstractmethod
     def add_gtruth_contour(self, ax, mode):
@@ -504,8 +462,6 @@ class twoD(Dataset):
                 'train' or 'test' (determines color).
         """
         pass
-
-
 
     def plot_train_imgs(self, inputs, labels):
         """ Plot training images (scatter plot) """
@@ -530,32 +486,29 @@ class twoD(Dataset):
 
         self.finalize_imgs(ax, 'train')
 
-
-
     def plot_test_imgs(self, inputs, probs):
         """ Plot output test probability map """
 
         check_device(inputs, probs, dev='cpu')
 
-        x_idx = (inputs[:, 0] + self.rect_side/2).div(self.test_grid).floor().to(torch.int64)
-        y_idx = (self.rect_side/2 - inputs[:, 1]).div(self.test_grid).floor().to(torch.int64)
+        x_idx = (inputs[:, 0] + self.rect_side / 2)\
+            .div(self.test_grid).floor().to(torch.int64)
+
+        y_idx = (self.rect_side / 2 - inputs[:, 1])\
+            .div(self.test_grid).floor().to(torch.int64)
 
         z = torch.zeros((self.grid_arange.size(0), self.grid_arange.size(0)))
         z[y_idx, x_idx] = probs
-        z = z.numpy() # probability value at (x, y) plot coordinates
-
-        inputs = inputs.numpy()
-        x, y = inputs[:, 0], inputs[:, 1] # (x, y) plot coordinates
+        z = z.numpy()  # probability value at (x, y) plot coordinates
 
         ax = plt.gca()
         self.add_gtruth_contour(ax, 'test')
 
         cm = plt.cm.get_cmap('GnBu')
-        plt.imshow(z, vmin=0, vmax=1, cmap=cm, extent=(-1, 1, -1, 1), aspect='equal')
+        plt.imshow(z, vmin=0, vmax=1, cmap=cm,
+                   extent=(-1, 1, -1, 1), aspect='equal')
 
         self.finalize_imgs(ax, 'test')
-
-
 
     def finalize_imgs(self, ax, mode):
         """ Finalize image plotting """
@@ -577,13 +530,15 @@ class twoD(Dataset):
         fig.set_size_inches(img_size, img_size)
 
         if self.save_imgs:
-            plt.savefig(os.path.join(self.log_dir_model, mode + '_img.pdf'), bbox_inches='tight')
+            plt.savefig(
+                os.path.join(self.log_dir_model, mode + '_img.pdf'),
+                bbox_inches='tight'
+            )
 
         if self.plot_imgs:
             plt.show()
 
         plt.close()
-
 
 
 class Circle(twoD):
@@ -592,9 +547,8 @@ class Circle(twoD):
     def __init__(self, **params):
         """ """
         super().__init__(**params)
-        self.radius_sq = self.rect_side ** 2 / (2 * math.pi) # circle_area = rect_area/2
-
-
+        # circle_area = rect_area/2
+        self.radius_sq = self.rect_side ** 2 / (2 * math.pi)
 
     def get_labels(self, inputs):
         """
@@ -610,8 +564,6 @@ class Circle(twoD):
 
         return labels
 
-
-
     def add_gtruth_contour(self, ax, mode):
         """
         Add contour of circle to plot.
@@ -625,11 +577,11 @@ class Circle(twoD):
         assert mode in ['train', 'test']
 
         color = 'black' if mode == 'train' else 'chocolate'
-        circle = plt.Circle((0, 0), radius=math.sqrt(self.radius_sq), color=color,
-                                linewidth=3, fill=False)
+        circle = plt.Circle((0, 0),
+                            radius=math.sqrt(self.radius_sq),
+                            color=color, linewidth=3, fill=False)
 
         ax.add_artist(circle)
-
 
 
 class S_shape(twoD):
@@ -638,10 +590,9 @@ class S_shape(twoD):
     def __init__(self, **params):
         """ """
         super().__init__(**params)
-        self.base_sin = lambda t : 0.4*np.sin(-5*t)
+        self.base_sin = lambda t: 0.4 * np.sin(-5 * t)
         self.sin_shift = 0.3
         self.y_cutoff = 0.8
-
 
     def sin_func(self, t, type_):
         """ Shifted sin_func for boundaries of s_shape.
@@ -657,8 +608,6 @@ class S_shape(twoD):
         else:
             return self.base_sin(t) - self.sin_shift
 
-
-
     def get_labels(self, inputs):
         """
         Generate dataset labels for a set of inputs.
@@ -671,14 +620,13 @@ class S_shape(twoD):
         x, y = inputs[:, 0].numpy(), inputs[:, 1].numpy()
 
         # within the two sinusoids
-        in_sin = np.logical_and(x > self.sin_func(y, 'lower'), x < self.sin_func(y, 'upper'))
+        in_sin = np.logical_and(x > self.sin_func(y, 'lower'),
+                                x < self.sin_func(y, 'upper'))
         in_boundaries = (np.abs(y) < self.y_cutoff)  # within y boundaries
 
         np_labels = np.logical_and(in_sin, in_boundaries).astype(np.float32)
 
         return torch.from_numpy(np_labels)
-
-
 
     def add_gtruth_contour(self, ax, mode):
         """
@@ -696,11 +644,15 @@ class S_shape(twoD):
 
         a = 0.001
         t_sin = np.arange(-self.y_cutoff, self.y_cutoff + a, a)
-        t_left = np.arange(self.sin_func(-self.y_cutoff, 'lower'), self.sin_func(-self.y_cutoff, 'upper') + a, a)
-        t_right = np.arange(self.sin_func(self.y_cutoff, 'lower'), self.sin_func(self.y_cutoff, 'upper') + a, a)
+        t_left = np.arange(self.sin_func(-self.y_cutoff, 'lower'),
+                           self.sin_func(-self.y_cutoff, 'upper') + a, a)
+        t_right = np.arange(self.sin_func(self.y_cutoff, 'lower'),
+                            self.sin_func(self.y_cutoff, 'upper') + a, a)
 
         ax.plot(self.sin_func(t_sin, 'lower'), t_sin, c=color, linewidth=3)
         ax.plot(self.sin_func(t_sin, 'upper'), t_sin, c=color, linewidth=3)
 
-        ax.plot(t_left, np.full_like(t_left, -self.y_cutoff),  c=color, linewidth=3)
-        ax.plot(t_right, np.full_like(t_right, self.y_cutoff), c=color, linewidth=3)
+        ax.plot(t_left, np.full_like(t_left, -self.y_cutoff),
+                c=color, linewidth=3)
+        ax.plot(t_right, np.full_like(t_right, self.y_cutoff),
+                c=color, linewidth=3)
