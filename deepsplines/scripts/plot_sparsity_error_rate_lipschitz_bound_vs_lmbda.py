3c6a11ce3ebf61ea64265bc4ac464bb2f1529d79
Joaquim Campos <joaquimcampos15@hotmail.com>
2021-07-30 13:59:43 +0200

    refactor(plot_sparsity_..._vs_lmbda): linting

--- deepsplines/scripts/plot_sparsity_error_rate_lipschitz_bound_vs_lmbda.py
+++ deepsplines/scripts/plot_sparsity_error_rate_lipschitz_bound_vs_lmbda.py
@@ -6,6 +6,7 @@ from functools import partial
 import numpy as np
 import matplotlib.pyplot as plt
 
+from deepsplines.manager import Manager
 from deepsplines.project import Project
 from deepsplines.ds_utils import json_load
 
@@ -16,10 +17,10 @@ in the same folder as the logged models.
 '''
 
 
-def plot_lmbda_vs_y(lmbdas, y, y_title, file_title):
+def plot_lmbda_vs_y(lmbdas, save_dir, y, y_title, file_title):
     """  """
-    ## lmbda vs sparsity
-    fig = plt.figure()
+    # lmbda vs sparsity
+    plt.figure()
     ax = plt.gca()
     ax.grid(True)
 
@@ -28,33 +29,20 @@ def plot_lmbda_vs_y(lmbdas, y, y_title, file_title):
 
     ax.plot(lmbdas, y, '--o', linewidth=1.0)
     ax.set_xscale('log')
-    ax.set_xlim([lmbdas.min()/10, lmbdas.max()*10])
+    ax.set_xlim([lmbdas.min() / 10, lmbdas.max() * 10])
 
-    if args.save_dir is not None:
-        plt.savefig(os.path.join(args.save_dir, file_title + '.pdf'))
+    if save_dir is not None:
+        plt.savefig(os.path.join(save_dir, file_title + '.pdf'))
 
     plt.show()
     plt.close()
 
 
-if __name__ == "__main__":
-
-    # parse arguments
-    parser = argparse.ArgumentParser(description='Plot sparsity, error rate and lipschitz bound '
-                                        'vs TV2/BV2 regularization weight.',
-                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('results_json', type=str, metavar='FILE[STR]',
-                        help='json file with train/test results.')
-    parser.add_argument('--save_dir', metavar='STR', type=str,
-                        help='directory for saving plots. If not given, plots are not saved.')
-    args = parser.parse_args()
-
-    if not os.path.isfile(args.results_json):
-        raise ValueError(f'File {args.results_json} does not exist.')
-
-    if args.save_dir is not None and not os.path.isdir(args.save_dir):
-        raise OSError(f'Save directory {save_dir} does not exist.')
-
+def plot_sparsity_error_rate_lipschitz_bound_vs_lmbda(args):
+    """
+    Args:
+        args: verified arguments from arparser
+    """
     log_dir = '/'.join(args.results_json.split('/')[:-1])
     results_dict = json_load(args.results_json)
 
@@ -71,7 +59,7 @@ if __name__ == "__main__":
         ckpt_filename = Project.get_ckpt_from_log_dir_model(log_dir_model)
         ckpt, params = Project.load_ckpt_params(ckpt_filename)
 
-        net  = Manager.build_model(params, device=device)
+        net = Manager.build_model(params, device=params['device'])
         net.load_state_dict(ckpt['model_state'], strict=True)
         net.eval()
 
@@ -85,12 +73,12 @@ if __name__ == "__main__":
         # try:
         #     sparsities[i] = eval(results_dict[model]['sparsity'])
         #     error_rates[i] = 100. - eval(results_dict[model][acc_str])
-        #     lipschitz_bounds[i] = eval(results_dict[model]['lipschitz_bound'])
+        #     lipschitz_bounds[i] = \
+        #         eval(results_dict[model]['lipschitz_bound'])
         # except KeyError:
         #     print(f'Model {model} did not log "sparsity", "{acc_str}" '
         #             'or "lipschitz_bound".')
         #     raise
-
 
     # sort according to lmbdas
     idx = np.argsort(lmbdas)
@@ -102,19 +90,50 @@ if __name__ == "__main__":
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
 
-    plot_func = partial(plot_lmbda_vs_y, lmbdas)
+    plot_func = partial(plot_lmbda_vs_y, lmbdas, args.save_dir)
 
-    ## lmbda vs sparsity
+    # lmbda vs sparsity
     y_title = "Number of non-sparse coefficients"
     file_title = 'sparsity_vs_lmbda'
     plot_func(sparsities, y_title, file_title)
 
-    ## lmbda vs error_rate
+    # lmbda vs error_rate
     y_title = r"Error$ \ $rate (\%)"
     file_title = 'error_rate_vs_lmbda'
     plot_func(error_rates, y_title, file_title)
 
-    ## lmbda vs lipschitz_bound
+    # lmbda vs lipschitz_bound
     y_title = r"Lipschitz$ \ $bound"
     file_title = 'lipschitz_bound_vs_lmbda'
     plot_func(np.absolute(lipschitz_bounds), y_title, file_title)
+
+
+if __name__ == "__main__":
+
+    # parse arguments
+    parser = argparse.ArgumentParser(
+        description='Plot sparsity, error rate and lipschitz bound '
+                    'vs TV2/BV2 regularization weight.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        'results_json',
+        metavar='FILE[STR]',
+        type=str,
+        help='json file with train/test results.'
+    )
+    parser.add_argument(
+        '--save_dir',
+        metavar='STR',
+        type=str,
+        help='directory for saving plots. If not given, plots are not saved.'
+    )
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.results_json):
+        raise ValueError(f'File {args.results_json} does not exist.')
+
+    if args.save_dir is not None and not os.path.isdir(args.save_dir):
+        raise OSError(f'Save directory {args.save_dir} does not exist.')
+
+    plot_sparsity_error_rate_lipschitz_bound_vs_lmbda(args)
